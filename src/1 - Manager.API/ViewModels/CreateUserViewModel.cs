514044988e466a6aa81aa9339ceafe6cce0772a8
Eschechola <lucas.eschechola@outlook.com>
2021-04-05 17:15:16 -0300

    Modify validation message in ViewModel

--- src/1 - Manager.API/ViewModels/CreateUserViewModel.cs
+++ src/1 - Manager.API/ViewModels/CreateUserViewModel.cs
@@ -16,7 +16,7 @@ namespace Manager.API.ViewModes{
         public string Email { get; set; }
         
         [Required(ErrorMessage = "A senha não pode ser vazia.")]
-        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres.")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 10 caracteres.")]
         [MaxLength(80, ErrorMessage = "A senha deve ter no máximo 80 caracteres.")]
         public string Password { get; set; }
     }
