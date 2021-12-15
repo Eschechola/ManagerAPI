4edd4cdb1c95b568fc6fd810cd6047f00af2d80e
Lucas Eschechola <lucas.eschechola@outlook.com>
2021-12-14 21:26:44 -0300

    Replace exception by domain notifications

--- src/3 - Manager.Services/Services/UserService.cs
+++ src/3 - Manager.Services/Services/UserService.cs
@@ -2,37 +2,52 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using EscNet.Cryptography.Interfaces;
-using Manager.Core.Exceptions;
+using Manager.Core.Communication.Mediator.Interfaces;
+using Manager.Core.Communication.Messages.Notifications;
+using Manager.Core.Enum;
+using Manager.Core.Validations.Message;
 using Manager.Domain.Entities;
 using Manager.Infra.Interfaces;
 using Manager.Services.DTO;
 using Manager.Services.Interfaces;
 
-namespace Manager.Services.Services{
+namespace Manager.Services.Services
+{
     public class UserService : IUserService{
         private readonly IMapper _mapper;
         private readonly IUserRepository _userRepository;
         private readonly IRijndaelCryptography _rijndaelCryptography;
+        private readonly IMediatorHandler _mediator;
 
         public UserService(
             IMapper mapper,
             IUserRepository userRepository,
-            IRijndaelCryptography rijndaelCryptography)
+            IRijndaelCryptography rijndaelCryptography, 
+            IMediatorHandler mediator)
         {
             _mapper = mapper;
             _userRepository = userRepository;
             _rijndaelCryptography = rijndaelCryptography;
+            _mediator = mediator;
         }
 
         public async Task<UserDTO> Create(UserDTO userDTO){
             var userExists = await _userRepository.GetByEmail(userDTO.Email);
 
-            if(userExists != null)
-                throw new DomainException("Já existe um usuário cadastrado com o email informado.");
+            if (userExists != null)
+                await _mediator.PublishDomainNotificationAsync(new DomainNotification(
+                    ErrorMessages.UserAlreadyExists,
+                    DomainNotificationType.UserAlreadyExists));
 
             var user = _mapper.Map<User>(userDTO);
             user.Validate();
-            user.ChangePassword(_rijndaelCryptography.Encrypt(user.Password));
+            
+            if(!user.IsValid)
+                await _mediator.PublishDomainNotificationAsync(new DomainNotification(
+                   ErrorMessages.UserInvalid(user.ErrorsToString()),
+                   DomainNotificationType.UserInvalid));
+
+            user.SetPassword(_rijndaelCryptography.Encrypt(user.Password));
 
             var userCreated = await _userRepository.Create(user);
 
@@ -43,11 +58,19 @@ namespace Manager.Services.Services{
             var userExists = await _userRepository.Get(userDTO.Id);
 
             if (userExists == null)
-                throw new DomainException("Não existe nenhum usuário com o id informado!");
+                await _mediator.PublishDomainNotificationAsync(new DomainNotification(
+                   ErrorMessages.UserNotFound,
+                   DomainNotificationType.UserNotFound));
 
             var user = _mapper.Map<User>(userDTO);
             user.Validate();
-            user.ChangePassword(_rijndaelCryptography.Encrypt(user.Password));
+
+            if(!user.IsValid)
+                await _mediator.PublishDomainNotificationAsync(new DomainNotification(
+                   ErrorMessages.UserInvalid(user.ErrorsToString()),
+                   DomainNotificationType.UserInvalid));
+
+            user.SetPassword(_rijndaelCryptography.Encrypt(user.Password));
 
             var userUpdated = await _userRepository.Update(user);
 
